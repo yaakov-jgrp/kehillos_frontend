a8f999367d85fa6b3a789b3c4fdaf90bbad5ca0d
ishwar6 <ishwarj116@gmail.com>
2023-08-17 22:42:45 +0530

    SMTP issue fix for seen/unseen issue

--- backend/netfree/settings.py
+++ backend/netfree/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
-import environ
 from dotenv import load_dotenv
-load_dotenv
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
