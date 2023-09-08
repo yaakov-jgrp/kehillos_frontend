5c14322d53b9a49e4e5c9b3d5cf8b1d491c9158d
ishwar6 <ishwarj116@gmail.com>
2023-09-08 17:30:21 +0530

    Changes

--- backend/netfree/cron.py
+++ backend/netfree/cron.py
@@ -1,8 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from crm.views import ReadEmail
 from apscheduler.triggers.cron import CronTrigger
-import time
-import logging
 read_email = ReadEmail()
 
 
@@ -10,28 +8,8 @@ def test():
     print("JHGYUGYUGU")
 
 
-def run_scheduler():
-    try:
-        scheduler = BackgroundScheduler()
-        # scheduler.add_job(test, CronTrigger.from_crontab('* * * * *'))
-        scheduler.add_job(read_email.read_email_from_gmail, CronTrigger(second='*/10'))
-        scheduler.start()
-
-        while True:
-            # Keep the main thread running
-            time.sleep(1)
-
-    except Exception as e:
-        # Handle exceptions and log errors
-        logging.error(f"Scheduler crashed: {str(e)}")
-
-        # Sleep for a while before restarting (to avoid constant restarts)
-        time.sleep(60)  # Sleep for 60 seconds (adjust as needed)
-        run_scheduler()  # Restart the scheduler
-
 def Cronjob():
-    # Start the scheduler loop in a separate thread
-    import threading
-    scheduler_thread = threading.Thread(target=run_scheduler)
-    scheduler_thread.daemon = True
-    scheduler_thread.start()
+    scheduler = BackgroundScheduler()
+    # scheduler.add_job(test, CronTrigger.from_crontab('* * * * *'))
+    scheduler.add_job(read_email.read_email_from_gmail, CronTrigger(second='*/10'))
+    scheduler.start()
