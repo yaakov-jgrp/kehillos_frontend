e4c2dd94be21e3a36c65104342350b60f75fc6d0
ishwar6 <ishwarj116@gmail.com>
2023-10-02 23:31:50 +0530

    changes

--- frontend/src/services/request.js
+++ frontend/src/services/request.js
@@ -1,8 +1,9 @@
 import api from "./api";
 
 const getRequests = () => {
+  const lang = localStorage.getItem("DEFAULT_LANGUAGE")
     return api
-      .get('/api/crm/requests/')
+      .get(`/api/crm/requests/?lang=${lang}`)
   }
 
   const requestService = {
