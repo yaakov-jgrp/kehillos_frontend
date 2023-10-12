4ce7eaa8a923c8a750668fcc9866450678965ed1
ishwar6 <iisudrj11@gmail.com>
2023-10-12 19:42:57 +0530

    Profile feature

--- frontend/src/services/category.js
+++ frontend/src/services/category.js
@@ -2,34 +2,41 @@ import api from "./api";
 
 const getCategories = () => {
   const lang = localStorage.getItem("DEFAULT_LANGUAGE");
-  return api.get(`/api/crm/category/?lang=${lang}`);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.get(`/api/crm/category/?lang=${lang}&profile=${filterProfileID ? filterProfileID : "1"}`);
 };
 
 const searchSiteSetting = (query) => {
   const lang = localStorage.getItem("DEFAULT_LANGUAGE");
-  return api.get(`/api/crm/category/?search=${query}&lang=${lang}`);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.get(`/api/crm/category/?search=${query}&lang=${lang}&profile=${filterProfileID ? filterProfileID : "1"}`);
 };
 
 const getActions = () => {
   const lang = localStorage.getItem("DEFAULT_LANGUAGE");
-  return api.get(`/api/crm/actions/?lang=${lang}`);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.get(`/api/crm/actions/?lang=${lang}&profile=${filterProfileID ? filterProfileID : "1"}`);
 };
 
 const setDefaultAction = (data) => {
-  return api.post("/api/crm/actions/", data);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.post(`/api/crm/actions/?profile=${filterProfileID ? filterProfileID : "1"}`, data);
 };
 
 const getDefaultAction = () => {
   const lang = localStorage.getItem("DEFAULT_LANGUAGE");
-  return api.get(`/api/crm/actions/?get_default=true&lang=${lang}`);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.get(`/api/crm/actions/?get_default=true&lang=${lang}&profile=${filterProfileID ? filterProfileID : "1"}`);
 };
 
 const getDefaultTraffic = () => {
-  return api.get("/api/crm/netfree-traffic/?default=true");
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.get(`/api/crm/netfree-traffic/?default=true&profile=${filterProfileID ? filterProfileID : "1"}`);
 };
 
 const updateNetfreeTraffic = (data) => {
-  return api.post("/api/crm/netfree-traffic/", data);
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
+  return api.post(`/api/crm/netfree-traffic/?profile=${filterProfileID ? filterProfileID : "1"}`, data);
 };
 
 const updateCategories = () => {
@@ -37,12 +44,25 @@ const updateCategories = () => {
 };
 
 const updateActionInCategory = (data, id) => {
+  const filterProfileID = localStorage.getItem("FILTER_PROFILE_ID");
   if (id) {
-    return api.put(`/api/crm/category/?status=update&id=${id}`, data);
+    return api.put(`/api/crm/category/?status=update&id=${id}&profile=${filterProfileID ? filterProfileID : "1"}`, data);
   } else {
-    return api.put("/api/crm/category/", data);
+    return api.put(`/api/crm/category/?profile=${filterProfileID ? filterProfileID : "1"}`, data);
   }
 };
+
+const getProfilesList = (params) => {
+  return api.get(`/api/crm/netfree-categories-profile/${params}`)
+}
+
+const createFilterProfile = (data) => {
+  return api.post(`/api/crm/netfree-categories-profile/`, data)
+}
+
+const updateFilterProfile = (data, params) => {
+  return api.put(`/api/crm/netfree-categories-profile/${params}`, data)
+}
 
 const categoryService = {
   getCategories,
@@ -54,5 +74,8 @@ const categoryService = {
   updateNetfreeTraffic,
   updateActionInCategory,
   searchSiteSetting,
+  getProfilesList,
+  createFilterProfile,
+  updateFilterProfile
 };
 export default categoryService;
--- frontend/src/views/NetFree.jsx
+++ frontend/src/views/NetFree.jsx
@@ -10,6 +10,9 @@ import {
 import Loader from "../component/common/Loader";
 import { debounce } from "lodash";
 import ActionModal from "../component/category/ActionModal";
+import EditButtonIcon from "../component/common/EditButton";
+import moment from 'moment';
+import ProfileModal from "../component/category/ProfileModal";
 
 const NetFree = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -27,6 +30,11 @@ const NetFree = () => {
     const [siteSearch, setSiteSearch] = useState('');
     const [editActionID, setEditActionId] = useState(null);
     const [defaultTraffic, setDefaultTraffic] = useState(null);
+    const [profilesList, setProfilesList] = useState(null);
+    const [profileActiveIndex, setProfileActiveIndex] = useState(0);
+    const [activeProfile, setActiveprofile] = useState(null);
+    const [showProfileModal, setShowProfileModal] = useState(false);
+    const [newProfile, setNewProfile] = useState(true);
     const setResponseDataToState = (res) => {
         const response = res.data.data.map(el => {
             el.isActionUpdateEnabled = false;
@@ -209,10 +217,28 @@ const NetFree = () => {
         setIsLoading(false);
     }
 
+    const getAllProfilesListHandler = async () => {
+        const profilesListData = await categoryService.getProfilesList("");
+        setProfilesList(profilesListData.data.data);
+        setActiveprofile(profilesListData.data.data[0]);
+    }
+
+    const profileClickHandler = (index) => {
+        const newActiveProfile = profilesList?.filter((profile, i) => i === index);
+        localStorage.setItem("FILTER_PROFILE_ID", newActiveProfile[0].id);
+        setActiveprofile(newActiveProfile[0]);
+        setProfileActiveIndex(index);
+        setNewProfile(false);
+        getCategoryData();
+        getActionsList();
+        getDefaultTraffic();
+    }
+
     useEffect(() => {
         getCategoryData();
         getActionsList();
         getDefaultTraffic();
+        getAllProfilesListHandler();
     }, []);
 
     const ActionSelectBox = ({ options, categoryName, categoryId, currentActions, operationType, previousValue }) => {
@@ -231,6 +257,7 @@ const NetFree = () => {
             </div>
         );
     }
+
     return (
         <div className="md:h-full w-full flex-col-reverse md:flex-row flex gap-4">
             <ActionModal
@@ -243,8 +270,41 @@ const NetFree = () => {
                 editActionID={editActionID}
                 setEditActionId={setEditActionId}
             />
-            <div className="bg-white rounded-3xl w-full md:w-[calc(100%-260px)]">
-                <h3 className='py-4 px-7 font-bold text-[#2B3674]'>{t('netfree.categories')}</h3>
+            <ProfileModal
+                showModal={showProfileModal}
+                setShowModal={() => setShowProfileModal(!showProfileModal)}
+                profile={activeProfile}
+                newProfile={newProfile}
+                onClick={getAllProfilesListHandler}
+            />
+            <div className="bg-white rounded-3xl overflow-y-clip w-full md:w-[calc(100%-260px)]">
+                <div className="m-5 px-2">
+                    <ul className="relative pr-[150px] flex text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                        {
+                            profilesList && profilesList.length > 0 && profilesList.map((profile, index) => {
+                                return (
+                                    <li>
+                                        <a onClick={() => profileClickHandler(index)} className={`mr-1 inline-block cursor-pointer capitalize p-2 text-[#2B3674] rounded-t-sm ${profileActiveIndex === index ? "dark:bg-gray-800 bg-gray-100 dark:text-blue-500" : "hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"}`}>{profile.name}</a>
+                                    </li>
+                                )
+                            })
+                        }
+                        <li className="addFilterProfile">
+                            <a onClick={() => {
+                                setShowProfileModal(!showProfileModal);
+                                setNewProfile(true);
+                            }} className={`inline-block cursor-pointer capitalize p-2 text-cyan-600 bg-gray-100 rounded-t-sm active dark:bg-gray-800 dark:text-blue-500`}>+ Add Filter Profile</a>
+                        </li>
+                    </ul>
+                </div>
+                {
+                    activeProfile &&
+                    <div className="bg-gray-100 p-2 rounded-md min-w-[50%] min-h-[50px] max-h-[150px] overflow-y-auto max-w-[50%] mx-6 my-4">
+                        <div className="flex"><p className="text-xl capitalize">{activeProfile.name} Filter Profile</p><EditButtonIcon extra="mx-2" onClick={() => { setNewProfile(false); setShowProfileModal(!showProfileModal); }} /></div>
+                        <p className="text-xs">{activeProfile.description}</p>
+                        <p className="text-xs text-gray-600">Last updated at {moment(activeProfile.updated_at).format('MMM Do YYYY, h:mm A')}</p>
+                    </div>
+                }
                 {
                     isLoading &&
                     <div className='h-[calc(100%-36px)] w-full flex justify-center items-center'>
@@ -323,8 +383,8 @@ const NetFree = () => {
                     </table>
                 </div>
             </div>
-            <div className="flex flex-col gap-3 h-full w-full md:!min-w-[250px] md:!w-[250px]">
-                <div className="bg-white h-[30%] rounded-3xl text-center text-[#2B3674]">
+            <div className="flex flex-col gap-3 max-h-[100%] overflow-y-auto w-full md:!min-w-[250px] md:!w-[250px]">
+                <div className="bg-white min-h-[160px] max-h-[160px] rounded-3xl text-center text-[#2B3674]">
                     <h3 className="p-3 text-[22px] font-bold">{t('netfree.searchSiteSetting')}</h3>
                     <SearchField
                         variant="auth"
@@ -355,9 +415,9 @@ const NetFree = () => {
                         {t('netfree.updateCategoryButton')}
                     </button>
                 </div>
-                <div className="flex flex-col px-3 py-3 overflow-x-hidden items-start bg-white h-[37%] rounded-3xl text-center text-[#2B3674]">
+                <div className="flex flex-col px-3 py-3 overflow-x-hidden items-start bg-white max-h-[200px] min-h-[200px] rounded-3xl text-center text-[#2B3674]">
                     <h5 className="font-bold text-[20px]">{t('netfree.defaultAction')}</h5>
-                    <div className="max-h-[calc(100%-30px)] overflow-y-auto mb-2">
+                    <div className="max-h-[calc(100%-30px)] w-full overflow-y-auto mb-2">
                         {
                             defaultActionList.map(el => {
                                 return (
@@ -372,9 +432,16 @@ const NetFree = () => {
                     }} />
                 </div>
                 {defaultTraffic !== null &&
-                    <div className="flex justify-around items-center py-1 px-2 overflow-hidden items-start bg-white rounded-3xl text-center text-[#2B3674]">
-                        <h5 className="font-bold ml-2 text-[14px]">{t('netfree.trafficRecord')}</h5>
-                        <ToggleSwitch clickHandler={updateDefaultTrafficHandler} selected={defaultTraffic} />
+                    <div className="max-h-[150px] min-h-[150px] flex flex-col items-start py-1 px-2 overflow-hidden bg-white rounded-3xl text-center text-[#2B3674]">
+                        <div className="flex justify-around items-center w-full">
+                            <h5 className="font-bold ml-2 text-[14px]">{t('netfree.trafficRecord')}</h5>
+                            <ToggleSwitch clickHandler={updateDefaultTrafficHandler} selected={defaultTraffic} />
+                        </div>
+                        <div className="pl-2">
+                            <AddButtonIcon extra={''} onClick={() => {
+                                enableActionUpdate();
+                            }} />
+                        </div>
                     </div>
                 }
                 <div className="py-3 bg-white h-[23%] rounded-3xl text-center text-[#2B3674]">
