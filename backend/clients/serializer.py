dbb3e80271560ea3d0417eccb943502bd24b95a5
ishwar6 <ishwarj116@gmail.com>
2023-10-17 19:48:28 +0530

    serializers response changes

--- backend/clients/serializer.py
+++ backend/clients/serializer.py
@@ -2,6 +2,10 @@ from clients.models import NetfreeUser
 from rest_framework import serializers
 
 class NetfreeUserSerializer(serializers.ModelSerializer):
+    netfree_profile = serializers.SerializerMethodField()
     class Meta:
         model = NetfreeUser
         fields = '__all__'
+    
+    def get_netfree_profile(self,obj):
+        return obj.netfree_profile.name
