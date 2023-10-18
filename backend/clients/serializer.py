5428dd0c5c41a5cef0bf32df0c0de3b14690f4c2
ishwar6 <ishwarj116@gmail.com>
2023-10-18 19:30:32 +0530

    serializer response changes

--- backend/clients/serializer.py
+++ backend/clients/serializer.py
@@ -2,6 +2,17 @@ from clients.models import NetfreeUser
 from rest_framework import serializers
 
 class NetfreeUserSerializer(serializers.ModelSerializer):
+    user_id = serializers.CharField(max_length=100)
+    email = serializers.CharField(max_length=200)
     class Meta:
         model = NetfreeUser
         fields = '__all__'
+    def validate_user_id(self, value):
+        if NetfreeUser.objects.filter(user_id=value).exists():
+            raise serializers.ValidationError(f"This Client with user id {str(value)} already in use.")
+        return value
+
+    def validate_email(self, value):
+        if NetfreeUser.objects.filter(email=value).exists():
+            raise serializers.ValidationError(f"This Client with email {str(value)} already in use.")
+        return value
