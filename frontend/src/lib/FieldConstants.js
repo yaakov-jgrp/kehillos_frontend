80650876835e193413b9d824d80a2a5b5835926c
ishwar6 <iisudrj11@gmail.com>
2023-11-20 13:47:34 +0530

    Phone Data type

--- frontend/src/lib/FieldConstants.js
+++ frontend/src/lib/FieldConstants.js
@@ -1,7 +1,7 @@
 export const TextFieldConstants = ["text", "email"];
-export const NumberFieldConstants = ["decimal", "number"];
+export const NumberFieldConstants = ["decimal", "number", "phone"];
 export const DateFieldConstants = ["date"];
 export const checkBoxConstants = ["checkbox"];
 export const checkBoxValues = [true, false];
-export const dataTypes = ["text", "email", "decimal", "number", "date", "select", "checkbox"];
+export const dataTypes = ["text", "email", "decimal", "number", "phone", "date", "select", "checkbox"];
 export const BlockFieldCheckValues = ["required", "unique", "display"];
